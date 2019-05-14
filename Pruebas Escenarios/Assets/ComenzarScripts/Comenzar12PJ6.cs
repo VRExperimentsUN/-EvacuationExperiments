0db05eb9db426894b9a8d725fa30537f2c36f0b7
vrlls <seviloria@uninorte.edu.co>
2019-05-14 18:52:00 -0500

    nuevo anvance

---
+++ Pruebas Escenarios/Assets/ComenzarScripts/Comenzar12PJ6.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Comenzar12PJ6 : MonoBehaviour {
+
+	public Animator anim;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(this.transform.position.z >= -1.91085 && this.transform.position.z <= -1.614){ 
+			anim.SetFloat ("Empezar", 1.0f);
+
+
+			}else{
+				anim.SetFloat ("Empezar", 0.0f);
+
+		}
+
+		print ("pj6 " + this.transform.position.z);
+	}
+}
