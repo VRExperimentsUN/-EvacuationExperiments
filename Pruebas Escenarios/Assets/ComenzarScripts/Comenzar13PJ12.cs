86b6dfe51527d863ff5cdb8f474239c083988f48
vrlls <seviloria@uninorte.edu.co>
2019-05-17 22:42:28 -0500

    Ajuste

--- Pruebas Escenarios/Assets/ComenzarScripts/Comenzar13PJ12.cs
+++ Pruebas Escenarios/Assets/ComenzarScripts/Comenzar13PJ12.cs
@@ -11,7 +11,7 @@ public class Comenzar13PJ12 : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		if (this.transform.position.z <= -16.3 && this.transform.position.z>=-16.92) {
+		if (this.transform.position.z <= -14.7 && this.transform.position.z>=-16.5) {
 			anim.SetFloat ("Empezar", 1.0f);
 		} else {
 			anim.SetFloat ("Empezar", 0.0f);
