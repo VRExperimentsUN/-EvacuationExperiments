79b63bd8174d3cb69e0a70db454dbd8cec84ffa7
vrlls <seviloria@uninorte.edu.co>
2019-05-18 10:38:14 -0500

    Nuevo avance escenario 4

---
+++ Pruebas Escenarios/Assets/Comenzar14PJ13.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Comenzar14PJ13 : MonoBehaviour {
+    public Animator anim;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        print(this.transform.position.x+" PJ13");
+	}
+}
