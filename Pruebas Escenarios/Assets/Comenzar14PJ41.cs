e48468a338579a4bbbb00ed3de3a0cbca1567501
vrlls <seviloria@uninorte.edu.co>
2019-05-22 15:47:57 -0500

    Escenario 1_4

---
+++ Pruebas Escenarios/Assets/Comenzar14PJ41.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Comenzar14PJ41 : MonoBehaviour {
+    public Animator anim;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (this.transform.position.x >= -6.14 && this.transform.position.x<=-5.11)
+        {
+            anim.SetFloat("Empezar", 1.0f);
+        }
+        else
+        {
+            anim.SetFloat("Empezar", 0.0f);
+        }
+	}
+}
